46de029d398fbe44173dda017b0d95e478c14f4d
Javier Serna <javiserna7@gmail.com>
2020-09-14 20:43:57 +0200

    añadida la logica

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { Cuestionario } from './components'
 
-const API_URL = 'https://opentdb.com/api.php?amount=10&category=14&difficlty=easy';
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=14&difficlty=easy&type=multiple';
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+
 
   useEffect(() => {
     fetch(API_URL)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         setQuestions(data.results);
-      })
-  }, [])
+      });
+
+  }, []);
+
+  const handleAnswer = (answer) => {
+    const newIndex = currentIndex + 1
+    setCurrentIndex(newIndex)
+
+    if (answer === questions[currentIndex].correct_answer) {
+      setScore(score + 1);
+    }
+
+  }
 
 
   return questions.length > 0 ? (
     <div className="container">
-      <Cuestionario data={questions[0]} />
+      {currentIndex >= questions.length ? (
+        <h1 className="text-3xl font-bold text-white">Your score was {score}</h1>) :
+        (<Cuestionario data={questions[currentIndex]} handleAnswer={handleAnswer} />
+
+        )}
     </div>
   ) : (
       <h2 className="text-2xl font-bold">Loading...</h2 >
--- src/components/Cuestionario.js
+++ src/components/Cuestionario.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-const Button = ({ answer }) => (
-    <button className="bg-white p-4
-    text-puple-800 font-semibold rounded shadow">
-        {answer}
-    </button>
-)
 
-const Cuestionario = ({ data: { question, correct_answer, incorrect_answers }, }) => {
-    const shuffledAnswer = [correct_answer, ...incorrect_answers]
+
+const Cuestionario = ({ handleAnswer, data: { question, correct_answer, incorrect_answers }, }) => {
+    const shuffledAnswers = [correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5);
+    console.log(shuffledAnswers);
+
 
     return (
         <div>
@@ -19,10 +16,16 @@ const Cuestionario = ({ data: { question, correct_answer, incorrect_answers }, }
                 />
             </div>
             <div className=" grid grid-cols-2 gap-6 mt-6 ">
-                <Button onClick={() => shuffledAnswer[0]} answer={correct_answer} />
-                <Button onClick={() => shuffledAnswer[1]} answer={incorrect_answers[0]} />
-                <Button onClick={() => shuffledAnswer[2]} answer={incorrect_answers[1]} />
-                <Button onClick={() => shuffledAnswer[3]} answer={incorrect_answers[2]} />
+                {shuffledAnswers.map(answer => (
+
+                    <button
+                        className='bg-white p-4
+                        text-puple-800 font-semibold rounded shadow'
+                        onClick={() => handleAnswer(answer)}
+                        dangerouslySetInnerHTML={{ __html: answer }}
+                    />
+
+                ))}
             </div>
         </div>
 
