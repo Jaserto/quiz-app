d2ae5762ff1ba1521d3fb6043a0f68ee62e8320f
Javier Serna <javiserna7@gmail.com>
2020-09-15 03:09:36 +0200

    Updates

---
+++ precache-manifest.47f321a89c847b99990893a7b979ac97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168995f939b8af067eac086598704734",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "ad990bf02b6290c63420",
+    "url": "/quiz-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "88a212f4cbf94896ceaa",
+    "url": "/quiz-app/static/js/2.73feb4d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.73feb4d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad990bf02b6290c63420",
+    "url": "/quiz-app/static/js/main.472462a3.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
